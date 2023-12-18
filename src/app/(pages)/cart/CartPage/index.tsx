fcb86ee3cd89478e6f5c16c6316a40c630f8e5cb
Moses <moses.kumie@outlook.com>
2023-12-18 14:09:49 +1000

    ignore typescript (only for now3!)

--- src/app/(pages)/cart/CartPage/index.tsx
+++ src/app/(pages)/cart/CartPage/index.tsx
@@ -84,7 +84,7 @@ export const CartPage: React.FC<{
                 {/* CART ITEM LIST */}
 
                 <ul className={classes.itemsList}>
-                  {cart?.items?.map((item, index) => {
+                  {cart?.items?.map(item => {
                     if (typeof item.product === 'object') {
                       const {
                         quantity,
